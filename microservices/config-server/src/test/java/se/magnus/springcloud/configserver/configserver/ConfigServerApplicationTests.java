bedeb976e6adc239346a74587716d5b57d88d061
SYSTEMS\tarique.raheem <tarique.raheem@systemsltd.com>
2022-02-26 22:21:17 +0500

    Adding Spring Cloud Configuration Server to the landscape

---
+++ microservices/config-server/src/test/java/se/magnus/springcloud/configserver/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package se.magnus.springcloud.configserver.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
