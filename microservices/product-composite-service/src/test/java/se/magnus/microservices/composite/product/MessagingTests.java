e981fffabae5dde66e9881260d35405a6a055e75
SYSTEMS\tarique.raheem <tarique.raheem@systemsltd.com>
2022-03-04 10:24:17 +0500

    First Commit Replacing Spring Cloud Config Server with Kubernetes Config Maps and Secrets Replacing Spring Cloud Gateway with Ingress Resources Certificate Management Using CertManager & Let's Encrypt NGROK for tunneling from Let's Enctryp to local / minikube Kubernetes cluster

--- microservices/product-composite-service/src/test/java/se/magnus/microservices/composite/product/MessagingTests.java
+++ microservices/product-composite-service/src/test/java/se/magnus/microservices/composite/product/MessagingTests.java
@@ -38,7 +38,7 @@ import static se.magnus.microservices.composite.product.IsSameEvent.sameEventExc
 @SpringBootTest(
 	webEnvironment=RANDOM_PORT,
 	classes = {ProductCompositeServiceApplication.class, TestSecurityConfig.class },
-	properties = {"spring.main.allow-bean-definition-overriding=true","spring.cloud.config.enabled=false"})
+	properties = {"spring.main.allow-bean-definition-overriding=true"})
 public class MessagingTests {
 
     @Autowired
